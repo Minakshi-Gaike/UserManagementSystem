3f27e44d00cd3b768e7a21fe275809e219349e80
Minakshi-Gaike <minakshigaike@gmail.com>
2026-07-29 16:12:37 +0530

    Forgot Password implemented

--- UserManagement/UserManagement/DTOs/ForgotPasswordDTO.cs
+++ UserManagement/UserManagement/DTOs/ForgotPasswordDTO.cs
@@ -4,6 +4,8 @@ namespace UserManagementProject.DTOs
 {
     public class ForgotPasswordDto
     {
+        [Required(ErrorMessage = "Email Address is required")]
+        [EmailAddress(ErrorMessage = "Enter a valid email address")]
         public string EmailAddress { get; set; }
         //public string NewPaswword { get; set; }
         //public string ConfirmNewPassword {  get; set; }
